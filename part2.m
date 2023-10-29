ca1e3641591df886ebf4b9ba50d593af831d2463
Zichun (Jerry) Gao <zichungao88@gmail.com>
2023-10-29 15:24:23 -0500

    Set initial conditions

--- part2.m
+++ part2.m
@@ -14,9 +14,26 @@ ylabel('Percent of Total Population');
 ytickformat('percentage');
 
 % set initial conditions based on given cases & deaths data
+% initial phase: 3/18/2020 ~ 6/29/2021
 d = deaths_STL(1) / POP_STL;
 r = 0;
 i = cases_STL(1) / POP_STL;
 s = 1 - i - d;
 
-x = [s, i, r, d];
+x1 = [s, i, r, d];
+
+% delta phase: 6/30/2021 ~ 10/26/2021
+d = deaths_STL(68) / POP_STL;
+r = 0; % ?
+i = cases_STL(68) / POP_STL;
+s = 1 - i - d;
+
+x2 = [s, i, r, d];
+
+% omicron phase: 10/27/2021 ~ 3/22/2022
+d = deaths_STL(85) / POP_STL;
+r = 0; % ?
+i = cases_STL(85) / POP_STL;
+s = 1 - i - d;
+
+x3 = [s, i, r, d];
